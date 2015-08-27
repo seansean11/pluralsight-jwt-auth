684e3f3c5ae4204b9bc16df853a5d1ec08f745e1
Sean Michael <seankmichael@gmail.com>
2015-08-27 12:38:56 -0600

    initial commit

---
+++ app/scripts/app.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('pluralsightJwtAuthApp').config(function($stateProvider) {
+    $stateProvider
+
+    .state('main', {
+        url: '/',
+        templateUrl: '/views/main.html'
+    })
+
+    .state('register', {
+        url: '/register',
+        templateUrl: '/views/register.html'
+    });
+});
---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pluralsightJwtAuthApp
+ * @description
+ * # pluralsightJwtAuthApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pluralsightJwtAuthApp', ['ui.router']);
